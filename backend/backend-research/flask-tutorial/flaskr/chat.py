cae07a75a1c129455da400eef94292f0ba8908d5
Daniel <dawi2291@colorado.edu>
2025-06-18 16:04:26 +0800

    Added README.md Modified event name for connection event Added message to be printed when client disconnects from chatroom

--- backend/backend-research/flask-tutorial/flaskr/chat.py
+++ backend/backend-research/flask-tutorial/flaskr/chat.py
@@ -12,11 +12,14 @@ bp = Blueprint('chat', __name__)
 def chat():
     return render_template('chat/chat.html')
 
-@socketio.on('my event')
-def handle_my_custom_event(json):
-    print('connected')
+@socketio.on('connection event')
+def handle_my_custom_event(message):
+    print(message['data'])
 
 @socketio.on('message')
 def handle_message(message):
     send(message, broadcast=True)
-
+    
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected!')
