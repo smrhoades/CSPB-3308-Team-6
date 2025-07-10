7790ee6d25f0a6c7467c4f4b3e679aa4757e49ed
smrhaodes <srhoades@colorado.edu>
2025-07-10 04:14:35 +0000

    Only one history test

--- backend/tests/test_chat.py
+++ backend/tests/test_chat.py
@@ -5,5 +5,11 @@ def test_chat_page(client):
     response = client.get('/chat')
     assert response.status_code == 200
     
-def test_saveMessageToDb(client):
+def test_saveMessageToDb():
     #TODO
+    return
+    
+def test_history_page(client):
+    data = {'sender': 1,'receiver': 4}
+    response = client.post('/history', json=data)
+    assert response.status_code == 200
