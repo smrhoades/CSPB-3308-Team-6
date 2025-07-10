ac8f0a65fd3f6a07865b094de685d07a3057166f
smrhaodes <srhoades@colorado.edu>
2025-07-10 00:57:25 +0000

    Save messages to db on send

--- backend/message_app/chat.py
+++ backend/message_app/chat.py
@@ -1,8 +1,9 @@
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
-)
-from flask_socketio import send, emit
+import datetime
 
+from flask import (
+    Blueprint, g, render_template
+)
+from message_app.db import get_db
 from .__init__ import socketio
 
 bp = Blueprint('chat', __name__)
@@ -12,3 +13,20 @@ bp = Blueprint('chat', __name__)
 def chat():
     print("Started page")
     return render_template('chat/chat.html')
+
+# Saving a copy of the message to the database for the message history
+@socketio.on('database')
+def saveMessageToDb():
+    print('received message: ' + data)
+    db = get_db()
+    
+    msg = data.json['message']
+    sender = data.json['sender']
+    receiver = data.json['receiver']
+    now = datetime.datetime.now()
+            
+    print("Save to DB")
+    db.execute("INSERT INTO message_data VALUES (?, ?, ?, ?)",
+               (sender, receiver, msg, now))
+    db.commit()
+    return
--- backend/tests/test_chat.py
+++ backend/tests/test_chat.py
@@ -5,3 +5,5 @@ def test_chat_page(client):
     response = client.get('/chat')
     assert response.status_code == 200
     
+def test_saveMessageToDb(client):
+    #TODO
